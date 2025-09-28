fb69dbf0c5ca65230553b73098a046d77133ae65
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-09-28 23:16:14 +0700

    MovableObject.java

--- Arkanoid/MovableObject.java
+++ Arkanoid/MovableObject.java
@@ -5,6 +5,12 @@ abstract class MovableObject extends GameObject {
     // dx,dy lần lượt là tốc độ di chuyển của vật thể
     protected float dx, dy;
 
+    public MovableObject(float x, float y, float width, float height) {
+        super(x, y, width, height);
+        this.dx = 0;
+        this.dy = 0;
+    }
+
     // Method dùng để biểu diễu thao tác vật lý di chuyển của vật thể
     public void move() {
         x += dx;
