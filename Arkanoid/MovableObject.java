5bc47af89b0df492c771e36bc577812fc60daf55
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-09-28 22:54:53 +0700

    MovableObject.java

--- Arkanoid/MovableObject.java
+++ Arkanoid/MovableObject.java
@@ -1,8 +1,11 @@
 package Arkanoid;
-// Lớp cơ sở cho các đối tượng có thể di chuyển.
-abstract class MovableObject extends GameObject {
-    protected int dx, dy;
 
+// Lớp cơ sở cho các đối tượng có thể di chuyển kế thừa từ GameObject.
+abstract class MovableObject extends GameObject {
+    // dx,dy lần lượt là tốc độ di chuyển của vật thể
+    protected float dx, dy;
+
+    // Method dùng để biểu diễu thao tác vật lý di chuyển của vật thể
     public void move() {
         x += dx;
         y += dy;
