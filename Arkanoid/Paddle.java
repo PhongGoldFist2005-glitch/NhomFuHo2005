21246f99e76f36c6dc03ecddc6bd103bf1ef0680
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-09-28 23:11:17 +0700

    Paddle.java

--- Arkanoid/Paddle.java
+++ Arkanoid/Paddle.java
@@ -1,7 +1,21 @@
 package Arkanoid;
 
-public class Paddle extends MovableObject  {
+// class cho thanh đỡ bóng kế thừa từ MovableObject.
+public class Paddle extends MovableObject {
+	// Tốc độ của thanh đỡ
+	protected float speed;
+	// Tham số thể hiện nâng cấp cho thanh đỡ
+	protected float currentPowerUp;
 
+	Paddle(float x, float y, float high, float width, float speed, float currentPowerUp) {
+		this.x = x;
+		this.y = y;
+		this.high = high;
+		this.width = width;
+		this.speed = speed;
+		this.currentPowerUp = currentPowerUp;
+	}
+	
 	@Override
 	public void update() {
 		// TODO: Cập nhật vị trí hoặc trạng thái của thanh đỡ
@@ -12,4 +26,19 @@ public class Paddle extends MovableObject  {
 	public void render() {
 		// TODO: Vẽ thanh đỡ lên màn hình
 	}
-}
+
+	// Phương thức cho thanh di chuyển sang bên trái
+	public void moveLeft() {
+		// pass
+	}
+
+	// Phương thức cho thanh di chuyển sang bên phải
+	public void moveRight() {
+		// pass
+	}
+
+	// Phương thức nhằm nâng cấp thanh trượt
+	public void applyPowerUp() {
+		// pass
+	}
+ }
