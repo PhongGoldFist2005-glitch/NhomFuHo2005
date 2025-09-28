d8a941c226131a604edfc20e25e300ab47612c5f
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-09-28 23:18:22 +0700

    GameObject.java

--- Arkanoid/GameObject.java
+++ Arkanoid/GameObject.java
@@ -3,18 +3,25 @@ package Arkanoid;
 // Lớp trừu tượng chung cho mọi đối tượng trong game.
 abstract class GameObject {
     // Định nghĩa tọa đồ trong màn hình chơi
-    protected int x, y;
+    protected float x, y;
     // Giới hạn chiều rộng, chiều cao trong giao diện game
-    protected int width, height;
+    protected float width, height;
+
+    GameObject(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
     // Method dùng để update các thông tin trong gameplay
     public abstract void update() {
         // pass
-    };
+    }
 
     // Method dùng để hiện thị gameplay.
     public abstract void render() {
         // pass
-    };
+    }
 }
 
