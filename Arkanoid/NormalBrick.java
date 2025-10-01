8df40ca4a60a908edaca70bcfb0d52f818211665
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-10-01 09:31:48 +0700

    Update NormalBrick.java

--- Arkanoid/NormalBrick.java
+++ Arkanoid/NormalBrick.java
@@ -1,8 +1,12 @@
 package Arkanoid;
 
+// Xây dựng lớp gạch thường
 public class NormalBrick extends Brick {
     int hitPoints = 1;
     int type = 0;
 
-    public NormalBrick()
+    // Gạch thường chỉ có 1 máu và loại 0
+    public NormalBrick() {
+        super(this.hitPoints, this.type);
+    }
 }
