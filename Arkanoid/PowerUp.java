7bd72952291459d4bf97b85491a2f49af9dd27f7
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-10-01 09:56:51 +0700

    Update PowerUp.java

--- Arkanoid/PowerUp.java
+++ Arkanoid/PowerUp.java
@@ -1,9 +1,38 @@
 package Arkanoid;
 
-// Nâng cấp sức mạnh của quả bóng
+// Vât phẩm nâng cấp sức mạnh của quả bóng
 public class PowerUp extends GameObject {
     protected int type;
     protected float duration;
 
-    
+    // Xây dựng vật thể nâng cấp
+    public GameObject(float x, float y, float width, float height, int type, float duration) {
+        super(x, y, width, height);
+        this.type = type;
+        this.duration = duration;
+    }
+
+    // Xây dựng cách tiếp nhận power up
+    public void applyEffect(Paddle paddle) {
+        // pass
+    }
+
+    public void removeEffect(Paddle paddle) {
+        // pass
+    }
+
+    @Override
+    public void update() {
+        // pass
+    }
+
+    @Override
+    public void render() {
+        // pass
+    }
+
+    @Override
+    public void move() {
+        // pass
+    }
 }
