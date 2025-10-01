b0ced7fedc793e6f07b4a9e8fdd92ae28301e335
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-10-01 09:36:22 +0700

    Update StrongBrick.java

--- Arkanoid/StrongBrick.java
+++ Arkanoid/StrongBrick.java
@@ -1,5 +1,11 @@
 package Arkanoid;
 
+// Xây dựng lớp gạch có máu khỏe hơn
 public class StrongBrick extends Brick {
-    
+    protected int hitPoints = 2;
+    protected int type = 1;
+
+    public StrongBrick() {
+        super(this.hitPoints, this.type);
+    }
 }
