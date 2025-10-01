5bc462ec500d716087d1aef48901f27e8f3bc478
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-10-01 09:18:32 +0700

    Brick.java

--- Arkanoid/Brick.java
+++ Arkanoid/Brick.java
@@ -1,7 +1,21 @@
 package Arkanoid;
 
+// Xây dựng class Brick
 public class Brick extends GameObject {
+	// Thông tin chứa máu của viên gạch và loại gạch
+	private int hitPoints;
+	protected int type;
 
+	// Kiểm tra xem viên gạch đã bị tấn công bao nhiêu lần để trừ máu 
+	public void takeHit() {
+		// pass
+	}
+
+	// Kiểm tra xem viên gạch đã bị tiêu diệt hay chưa
+	public boolean isDestroyed() {
+		// pass
+	}
+	
 	@Override
 	public void update() {
 		// TODO: Cập nhật trạng thái của viên gạch nếu cần
