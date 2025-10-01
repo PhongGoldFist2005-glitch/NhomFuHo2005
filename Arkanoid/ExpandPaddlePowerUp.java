77211a1c9814566d7ee15722e38e91aeb0b57867
PhongGoldFist2005-glitch <23020855@vnu.edu.vn>
2025-10-01 10:02:15 +0700

    Update ExpandPaddlePowerUp.java

--- Arkanoid/ExpandPaddlePowerUp.java
+++ Arkanoid/ExpandPaddlePowerUp.java
@@ -1,5 +1,11 @@
 package Arkanoid;
 
 public class ExpandPaddlePowerUp extends PowerUp {
+    protected int type = 0;
+    protected float duration; // Có thể set làm hằng số
     
+    // Xây dựng vật thể nâng cấp
+    public GameObject(float x, float y, float width, float height) {
+        super(x, y, width, height, this.type, this.duration);
+    }
 }
